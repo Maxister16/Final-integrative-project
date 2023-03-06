a679c6f39c5b25f4dda137f07484950f8147b35a
Maxister16 <maximesevigny16@gmail.com>
2023-03-06 09:52:41 -0500

    Update the Table class

--- Pool/app/src/main/java/Pool/Table.java
+++ Pool/app/src/main/java/Pool/Table.java
@@ -1,4 +1,3 @@
-
 package Pool;
 
 import javafx.scene.image.Image;
@@ -7,11 +6,45 @@ import javafx.scene.shape.Rectangle;
 
 public class Table implements InteractiveObject{
     
-    Rectangle background;
-    Rectangle border;
+    final private double WIDTH = 500;
+    final private double HEIGHT = 250;
+    
+    private Rectangle background;
+    private Rectangle border;
     
     public Table(){
-       // this.setImage(newImage);
+        background = new Rectangle(WIDTH, HEIGHT);
+        border = new Rectangle(WIDTH, HEIGHT);
+        border.setFill(new ImagePattern(new Image("InteractiveObjectIMG\\tableBorderIMG.png")));
+    }
+    
+    public void setX(double x){
+        background.setX(x);
+        border.setX(x);
+    }
+    public void setY(double y){
+        background.setY(y);
+        border.setY(y);
+    }
+    public double getX(){
+        return background.getX();
+    }
+    public double getY(){
+        return background.getY();
+    }
+    public double getWidth(){
+        return background.getWidth();
+    }
+    public double getHeight(){
+        return background.getHeight();
+    }
+    
+    
+    public Rectangle getBorder(){
+        return this.border;
+    }
+    public Rectangle getBackground(){
+        return this.background;
     }
     
     @Override
