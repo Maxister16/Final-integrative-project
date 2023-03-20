491a47fac146929e2ec755e085010fefbbff4580
eszterannakovacs <eszteranna.kovacs@gmail.com>
2023-03-19 22:13:03 -0400

    Welcome page button animation done, more play page images added

--- Pool/app/src/main/java/Pool/ScenePlay.java
+++ Pool/app/src/main/java/Pool/ScenePlay.java
@@ -5,68 +5,105 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-    public class PlayScene {
-        private Scene scene;
+import java.awt.*;
 
-        public ScenePlay(Stage primaryStage) {
 
-            GameStatus.initialize();
+public class ScenePlay {
 
-            Pane gamePane = new Pane();
+            private Scene scene;
 
-            BorderPane borderPane = new BorderPane();
-            GridPane gridPaneTop = new GridPane();
-            GridPane gridPaneBot = new GridPane();
-            gridPaneTop.setAlignment(Pos.TOP_CENTER);
-            gridPaneBot.setAlignment(Pos.BOTTOM_LEFT);
+            public ScenePlay(Stage primaryStage) {
 
-            // Create Sliders and Buttons
-            Slider forceSlider = new Slider(1, 10, 1);
-            Slider angleSlider = new Slider(0.0, 359, 1.0);
-            Button playButton = new Button("Play");
-            Button menuButton = new Button("Menu");
+                GameStatus.initialize();
+                Pane gamePane = new Pane();
 
-            // Add Sliders and buttons to GridPane
-            gridPaneBot.add(menuButton, 0, 0);
-            gridPaneTop.add(forceSlider, 0, 0);
-            gridPaneTop.add(angleSlider, 2, 0);
-            gridPaneTop.add(playButton, 3, 0);
+                BorderPane borderPane = new BorderPane();
+                GridPane gridPaneTop = new GridPane();
+                GridPane gridPaneBot = new GridPane();
+                gridPaneTop.setAlignment(Pos.TOP_CENTER);
+                gridPaneBot.setAlignment(Pos.BOTTOM_LEFT);
 
-            // Create Layout
-            StackPane layout = new StackPane();
-            layout.setMinWidth(900);
-            layout.setMinHeight(600);
+                ImageView menu = new ImageView("ButtonIMG/MenuBtnIMG.png");
+                ImageView redPlay = new ImageView("ButtonIMG/RedPlayBtnIMG.png");
 
-            gamePane.getChildren().addAll(GameStatus.nets);
-            gamePane.getChildren().addAll(GameStatus.listOfBalls);
-            gamePane.getChildren().addAll(GameStatus.tableLines);
-            gamePane.getChildren().addAll(GameStatus.table.getBackground(),GameStatus.table.getBorder());
-            layout.getChildren().addAll(gridPaneTop, gridPaneBot,gamePane);
+                // Create Sliders and Buttons
+                Slider forceSlider = new Slider(1, 10, 1);
+                Slider angleSlider = new Slider(0.0, 359, 1.0);
+                Button playButton = new Button("", redPlay);
+                Button menuButton = new Button("",menu);
 
-            GameStatus.positionObjects(layout.getMinWidth(), layout.getMinHeight());
-            this.scene = new Scene(layout);
+                menu.setPreserveRatio(true);
+                menu.setFitHeight(50);
+                redPlay.setPreserveRatio(true);
+                redPlay.setFitHeight(70);
 
-            playButton.setOnAction((event) -> {
-                int force = (int)forceSlider.getValue();
-                int angle = (int)angleSlider.getValue();
-                System.out.println("Force: " + force + "Angle: " + angle);
-                //set values for force and angle
-            });
 
-            menuButton.setOnAction((event) -> {
-                SceneMenu sceneMenu = new SceneMenu(primaryStage);
-                primaryStage.setScene(sceneMenu.getScene());
-            });
+                //set button backgrounds and border transparent
+                menuButton.setBackground(null);
+                menuButton.setBorder(null);
+                playButton.setBackground(null);
+                playButton.setBorder(null);
+
+
+                // Add Sliders and buttons to GridPane
+                gridPaneBot.add(menuButton, 0, 0);
+                gridPaneTop.add(forceSlider, 0, 0);
+                gridPaneTop.add(angleSlider, 2, 0);
+                gridPaneTop.add(playButton, 3, 0);
+
+                // Create Layout
+                StackPane layout = new StackPane();
+
+                //?if I change these values the game Pane disappears
+                layout.setMinWidth(900);
+                layout.setMinHeight(600);
+
+                //Put table together
+                gamePane.getChildren().addAll(GameStatus.nets);
+                gamePane.getChildren().addAll(GameStatus.listOfBalls);
+                gamePane.getChildren().addAll(GameStatus.tableLines);
+                gamePane.getChildren().addAll(GameStatus.table.getBackground(), GameStatus.table.getBorder());
+
+                //Set background image
+
+
+                ImageView bg = new ImageView("BackgroundIMG/PlayBgIMG.jpg");
+
+                bg.setPreserveRatio(true);
+                bg.setFitWidth(1350);
+          //      backgroundPane.getChildren().add(bg);
+
+
+                //Position Panes
+                borderPane.getChildren().add(gamePane);
+                borderPane.setTop(gridPaneTop);
+                borderPane.setBottom(gridPaneBot);
+                layout.getChildren().addAll(bg,borderPane);
+
+                GameStatus.positionObjects(layout.getMinWidth(), layout.getMinHeight());
+                this.scene = new Scene(layout);
+
+                playButton.setOnAction((event) -> {
+                    int force = (int) forceSlider.getValue();
+                    int angle = (int) angleSlider.getValue();
+                    System.out.println("Force: " + force + "Angle: " + angle);
+                    //set values for force and angle
+                });
+
+                menuButton.setOnAction((event) -> {
+                    SceneMenu sceneMenu = new SceneMenu(primaryStage);
+                    primaryStage.setScene(sceneMenu.getScene());
+                });
+            }
+
+            public Scene getScene() {
+                return this.scene;
+            }
         }
-
-        public Scene getScene() {
-            return this.scene;
-        }
-    }
-
--- Pool/app/src/main/java/Pool/SceneWelcome.java
+++ Pool/app/src/main/java/Pool/SceneWelcome.java
@@ -1,15 +1,20 @@
 package Pool;
 
+import javafx.geometry.HPos;
 import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-import javax.swing.*;
+
+//To do: make volume on off button, add sound effects and music
 
 public class SceneWelcome {
     private Scene scene;
@@ -17,8 +22,24 @@ public class SceneWelcome {
     public SceneWelcome(Stage primaryStage) {
         GridPane gridPane = new GridPane();
         gridPane.setAlignment(Pos.CENTER);
-        gridPane.setVgap(10);
-        gridPane.setHgap(10);
+        gridPane.setTranslateY(90);
+        gridPane.setTranslateX(110);
+
+        // Create ColumnConstraints objects to set the size of the columns
+        ColumnConstraints column1 = new ColumnConstraints(200);
+        column1.setHalignment(HPos.CENTER);
+        ColumnConstraints column2 = new ColumnConstraints(200);
+        column2.setHalignment(HPos.CENTER);
+        ColumnConstraints column3 = new ColumnConstraints(200);
+        column3.setHalignment(HPos.CENTER);
+        RowConstraints row1 = new RowConstraints(200);
+        row1.setValignment(VPos.CENTER);
+
+
+        // Set the sizes of the columns and rows
+        gridPane.getColumnConstraints().addAll(column1, column2, column3);
+        gridPane.getRowConstraints().addAll(row1);
+
 
         //Set up Button ImageViews
         ImageView iceMode = new ImageView("ButtonIMG/IcePlayBtnIMG.png");
@@ -30,9 +51,6 @@ public class SceneWelcome {
         ImageView grassMode = new ImageView("ButtonIMG/GrassPlayBtnIMG.png");
         grassMode.setPreserveRatio(true);
         grassMode.setFitHeight(170);
-
-        gridPane.translateYProperty().bind(iceMode.fitHeightProperty().divide(2.15));
-        gridPane.translateXProperty().bind(iceMode.fitHeightProperty().divide(1.6));
 
 
         //Mode buttons with images
@@ -50,8 +68,8 @@ public class SceneWelcome {
 
         //Add buttons to gridPane
         gridPane.add(normalBtn, 0, 0);
-        gridPane.add(grassBtn, 2, 0);
-        gridPane.add(iceBtn, 3, 0);
+        gridPane.add(grassBtn, 1, 0);
+        gridPane.add(iceBtn, 2, 0);
 
         //Set up scene background
         StackPane backgroundPane = new StackPane();
@@ -72,13 +90,31 @@ public class SceneWelcome {
             ScenePlayNormal scenePlayNormal = new ScenePlayNormal(primaryStage);
             primaryStage.setScene(scenePlayNormal.getScene());
         });
+        normalBtn.setOnMouseEntered((event) -> {
+            normalMode.setFitHeight(185);
+        });
+        normalBtn.setOnMouseExited((event) -> {
+            normalMode.setFitHeight(170);
+        });
         grassBtn.setOnAction((event) -> {
             ScenePlayGrass scenePlayGrass = new ScenePlayGrass(primaryStage);
             primaryStage.setScene(scenePlayGrass.getScene());
         });
+        grassBtn.setOnMouseEntered((event) -> {
+            grassMode.setFitHeight(185);
+        });
+        grassBtn.setOnMouseExited((event) -> {
+            grassMode.setFitHeight(170);
+        });
         iceBtn.setOnAction((event) -> {
             ScenePlayIce scenePlayIce = new ScenePlayIce(primaryStage);
             primaryStage.setScene(scenePlayIce.getScene());
+        });
+        iceBtn.setOnMouseEntered((event) -> {
+            iceMode.setFitHeight(185);
+        });
+        iceBtn.setOnMouseExited((event) -> {
+            iceMode.setFitHeight(170);
         });
 
         //Styling found on google
